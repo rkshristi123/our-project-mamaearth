e2a4b058283e53d52858c03c94e71fc231d73194
Chandani99 <er.chandni99@gmail.com>
2022-04-01 03:16:55 +0530

    heder chenged

--- face.js
+++ face.js
@@ -1,67 +1,188 @@
-var home=document.querySelector("#home");
-home.addEventListener("click", homePage)
-function homePage(){
-  window.location.href="index.html";
+facedata=[
+  {image:"https://honasa-mamaearth-production.imgix.net/a/l/aloe-vera-face-serum.jpg?auto=compress&fit=scale&w=400&h=400",
+   name:"Aloe Vera Face Serum with Aloe Vera & Ashwagandha for a Youthful Glow– 30 ml",
+   rating:"4.8★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/a/l/aloevera_gel_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Aloe Ashwagandha Gel with Aloe Vera & Ashwagandha for a Youthful Glow - 300 ml ",
+   rating:"4.7★",
+   price:"₹349"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/a/l/aloevera-fw-1.jpg?auto=compress&fit=scale&w=400&h=400",
+   name:"Aloe Vera Face Wash with Aloe Vera & Ashwagandha for a Youthful Glow - 100 ml",
+
+   rating:"4.8★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/a/l/aloe-vera-night-cream.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Aloe Vera Night Cream with Aloe Vera and Ashwagandha for a Youthful Glow – 50 g",
+   rating:"4.8★",
+   price:"₹499"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/n/i/niacin-face-toner_1.jpg?auto=compress&fit=scale&w=400&h=400",
+   name:"Niacin Face Toner with Niacinamide and Witch Hazel for Acne and Open Pores - 200 ml",
+   rating:"4.4★",
+   price:"₹299"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/l/e/leaves-of-clarity-essence-serum_2_1_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Leaves of Clarity Essence Serum with Neem & Salicylic Acid for Clear Skin– 30 mll",
+   rating:"4.4★",
+   price:"₹699"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/b/y/bye-bye-open-pores-face-cream_1.jpg?auto=compress&fit=scale&w=400&h=400",
+   name:"Bye Bye Open Pores Face Cream with Rosehip & Niacinamide For Pore Tightening – 30 g",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/h/y/hydragel_indian_sunscreen_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+    name:"HydraGel Indian Sunscreen with Aloe Vera and Raspberry for Sun Protection - 50 g",
+   rating:"4.7★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_foaming_face_wash_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Foaming Face Wash with Vitamin C and Turmeric for Skin Illumination - 150ml ",
+   rating:"4.9★",
+   price:"₹499"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/f/l/flowers-of-youth-essence-serum_2_1_1_1.jpg?auto=compress&fit=scale&w=400&h=400",
+   name:"Garden of Glow Essence Serum with Vitamin C & Passion Fruit for Skin Illumination – 30 ml",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/b/y/bye_bye_blemishes_2_2.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Bye Bye Dark Circles Eye Cream with Cucumber and Peptides for Dark Circles - 20ml",
+   rating:"4.9★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan_face_mask_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Face Mask with Saffron and Turmeric for Skin Brightening and Tan Removal - 100g",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/o/i/oil-free-face-wash_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Oil-Free Face Wash with Apple Cider Vinegar & Salicylic Acid for Acne-Prone Skin– 100 ml",
+   rating:"4.7★",
+   price:"₹799"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan_face_mask_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Face Mask with Saffron and Turmeric for Skin Brightening and Tan Removal - 100g",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/c/h/charcoal_face_scrub_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Charcoal Face Scrub For Oily Skin and Normal skin, with Charcoal and Walnut for Deep Exfoliation - 100g",
+   rating:"4.9★",
+   price:"₹699"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/_/b/_bye_bye_wrinkles_face_cream_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Bye Bye Wrinkles Face Cream with Green Tea & Collagen for Wrinkles & Fine Lines – 30 g",
+   rating:"4.9★",
+   price:"₹499"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vit-c-under-eye-cream-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Under Eye Cream with Gotu Kola & Niacinamide for Brightening Under Eyes - 20 ml ",
+   rating:"4.9★",
+   price:"₹999"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/b/y/bye_bye_blemishes_1_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Bye Bye Blemishes Face Cream for Reducing Pigmentation and Blemishes with  Extract of Vitamin C -30ml ",
+   rating:"4.9★",
+   price:"₹799"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/c/h/charcoal_face_scrub_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Charcoal Face Scrub For Oily Skin and Normal skin, with Charcoal and Walnut for Deep Exfoliation - 100g ",
+   rating:"4.9★",
+   price:"₹799"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/r/o/rose_face_toner_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Rose Face Toner with Witch Hazel and Rose Water for Pore Tightening - 200ml",
+   rating:"4.7★",
+   price:"₹699"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/t/e/tea-tree-foaming-face-wash-with-ingredients_85zdnmuke0xpfych.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Tea Tree Foaming Face Wash with Tea Tree and Salicylic Acid for Acne and Pimples - 150ml",
+   rating:"4.9★",
+   price:"₹799"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/c/h/cherry-lip-balm_1_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Mamaearth Cherry Tinted 100% Natural Lip Balm with Cherry and Coconut Oil  for norishing lips- 2 g ",
+   rating:"4.9★",
+   price:"₹299"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-face-wash_2_1_8up0nipzzkyzwwx1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Face Wash with Vitamin C and Turmeric for Skin Illumination for brigthening skin- 250ml",
+   rating:"4.9★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/2/_/2_5.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Apple Cider Vinegar Foaming Face Wash with Apple Cider Vinegar and Aloe Vera  - 150ml ",
+   rating:"4.9★",
+   price:"₹799"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/2/_/2_4.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Foaming Face Wash with Turmeric and Saffron for Tan Removal and softens skin- 150ml",
+   rating:"4.9★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan-nightface-mask.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Night Face Mask with Turmeric and Niacinamide for Glowing Skin - 100 g ",
+   rating:"4.9★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan_face_mask_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Face Mask with Saffron and Turmeric for Skin Brightening and Tan Removal - 100g ",
+   rating:"4.9★",
+   price:"₹799"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/a/q/aqua-glow-gel-face-moisturizer-1_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Aqua Glow Gel Face Moisturizer With Himalayan Thermal Water and Hyaluronic Acid  - 100ml",
+   rating:"4.9★",
+   price:"₹899"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/1/_/1_1_.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Charcoal Black Long Stay Kajal with Vitamin C & Chamomile for 11-Hour Smudge-free Stay ",
+   rating:"4.9★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-lip-balm_1_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Mamaearth Vitamin C Tinted 100% Natural Lip Balm with Vitamin C & Honey - 2 g",
+   rating:"4.9★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_face_milk_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Face Milk with Vitamin C and Peach for Skin Illumination - 100 ml",
+   rating:"3.9★",
+   price:"₹499"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/r/o/rosehip-serum.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Rosehip Face Serum with Rosehip & Gotu Kola for Glowing Skin and soft skin – 30 ml",
+   rating:"4.9★",
+   price:"₹1399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/s/k/skin-correct-face-serum-with-ingredient_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Skin Correct Face Serum with Niacinamide and Ginger Extract and aloevera   - 30 ml",
+   rating:"4.9★",
+   price:"₹899"
+  },
  
-}
-var baby=document.querySelector("#baby");
-baby.addEventListener("click", babyPage)
-function babyPage(){
-  window.location.href="baby.html";
- 
-}
-var beauty=document.querySelector("#beauty");
-beauty.addEventListener("click", beautyPage)
-function beautyPage(){
-  window.location.href="beauty.html";
- 
-}
-var hair=document.querySelector("#hair");
-hair.addEventListener("click", hairPage)
-function hairPage(){
-  window.location.href="hair.html";
- 
-}
-var face=document.querySelector("#face");
-face.addEventListener("click", facePage)
-function facePage(){
-  window.location.href="face.html";
- 
-}
-var body=document.querySelector("#body");
-body.addEventListener("click", bodyPage)
-function bodyPage(){
-  window.location.href="body.html";
- 
-}
-var giftPacks=document.querySelector("#giftPacks");
-giftPacks.addEventListener("click", giftPacksPage)
-function giftPacksPage(){
-  window.location.href="giftpack.html";
- 
-}
-var allProducts=document.querySelector("#allProducts");
-allProducts.addEventListener("click", allProductsPage)
-function allProductsPage(){
-  window.location.href="allProducts.html";
- 
-}
-var blog=document.querySelector("#blog");
-blog.addEventListener("click", blogPage)
-function blogPage(){
-  window.location.href="blog.html";
- 
-}
-var plantGoodness=document.querySelector("#plantGoodness");
-plantGoodness.addEventListener("click", plantGoodnessPage)
-function plantGoodnessPage(){
-  window.location.href="plantGoodness.html";
- 
-}
-var storeLocator=document.querySelector("#storeLocator");
-storeLocator.addEventListener("click", storeLocatorPage)
-function storeLocatorPage(){
-  window.location.href="storeLocator.html";
- 
-}
-console.log("Hello");
+
+];
+facedata.map(function(elem){
+  var div=document.createElement("div")
+  var img=document.createElement("img");
+  img.src=elem.image;
+  var h3=document.createElement("p");
+  h3.innerText=elem.name;
+  var rating=document.createElement("div")
+  rating.innerText=elem.rating;
+  var price=document.createElement("h3")
+  price.innerText=elem.price;
+  var btn=document.createElement("button")
+  btn.innerText="ADD TO CART";
+  btn.setAttribute("class","btn")
+  div.append(img,h3,rating,price,btn)
+  document.querySelector("#container").append(div);
+
+
+});
