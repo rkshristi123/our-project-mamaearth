fafcb6ad12d0eb3d3f7bf3b23a89c4ec8b0ea247
rkshristi123 <shristikumari759@gmail.com>
2022-03-31 11:16:36 +0530

    added home js

---
+++ index.js
@@ -0,0 +1,67 @@
+var home=document.querySelector("#home");
+home.addEventListener("click", homePage)
+function homePage(){
+  window.location.href="header.html";
+ 
+}
+var baby=document.querySelector("#baby");
+baby.addEventListener("click", babyPage)
+function babyPage(){
+  window.location.href="baby.html";
+ 
+}
+var beauty=document.querySelector("#beauty");
+beauty.addEventListener("click", beautyPage)
+function beautyPage(){
+  window.location.href="beauty.html";
+ 
+}
+var hair=document.querySelector("#hair");
+hair.addEventListener("click", hairPage)
+function hairPage(){
+  window.location.href="hair.html";
+ 
+}
+var face=document.querySelector("#face");
+face.addEventListener("click", facePage)
+function facePage(){
+  window.location.href="face.html";
+ 
+}
+var body=document.querySelector("#body");
+body.addEventListener("click", bodyPage)
+function bodyPage(){
+  window.location.href="body.html";
+ 
+}
+var giftPacks=document.querySelector("#giftPacks");
+giftPacks.addEventListener("click", giftPacksPage)
+function giftPacksPage(){
+  window.location.href="header.html";
+ 
+}
+var allProducts=document.querySelector("#allProducts");
+allProducts.addEventListener("click", allProductsPage)
+function allProductsPage(){
+  window.location.href="allProducts.html";
+ 
+}
+var blog=document.querySelector("#blog");
+blog.addEventListener("click", blogPage)
+function blogPage(){
+  window.location.href="blog.html";
+ 
+}
+var plantGoodness=document.querySelector("#plantGoodness");
+plantGoodness.addEventListener("click", plantGoodnessPage)
+function plantGoodnessPage(){
+  window.location.href="plantGoodness.html";
+ 
+}
+var storeLocator=document.querySelector("#storeLocator");
+storeLocator.addEventListener("click", storeLocatorPage)
+function storeLocatorPage(){
+  window.location.href="storeLocator.html";
+ 
+}
+console.log("Hello");
