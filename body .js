63c80275b3d85628bcfdd7bc5359c5e77e5e5dff
Chandani99 <er.chandni99@gmail.com>
2022-04-01 03:21:41 +0530

    header and js changed

--- body .js
+++ body .js
@@ -1,67 +1,161 @@
-var home=document.querySelector("#home");
-home.addEventListener("click", homePage)
-function homePage(){
-  window.location.href="index.html";
- 
-}
-var baby=document.querySelector("#baby");
-baby.addEventListener("click", babyPage)
-function babyPage(){
-  window.location.href="baby.html";
- 
-}
-var beauty=document.querySelector("#beauty");
-beauty.addEventListener("click", beautyPage)
-function beautyPage(){
-  window.location.href="beauty.html";
- 
-}
-var hair=document.querySelector("#hair");
-hair.addEventListener("click", hairPage)
-function hairPage(){
-  window.location.href="hair.html";
- 
-}
-var face=document.querySelector("#face");
-face.addEventListener("click", facePage)
-function facePage(){
-  window.location.href="face.html";
- 
-}
-var body=document.querySelector("#body");
-body.addEventListener("click", bodyPage)
-function bodyPage(){
-  window.location.href="body.html";
- 
-}
-var giftPacks=document.querySelector("#giftPacks");
-giftPacks.addEventListener("click", giftPacksPage)
-function giftPacksPage(){
-  window.location.href="giftpack.html";
- 
-}
-var allProducts=document.querySelector("#allProducts");
-allProducts.addEventListener("click", allProductsPage)
-function allProductsPage(){
-  window.location.href="allProducts.html";
- 
-}
-var blog=document.querySelector("#blog");
-blog.addEventListener("click", blogPage)
-function blogPage(){
-  window.location.href="blog.html";
- 
-}
-var plantGoodness=document.querySelector("#plantGoodness");
-plantGoodness.addEventListener("click", plantGoodnessPage)
-function plantGoodnessPage(){
-  window.location.href="plantGoodness.html";
- 
-}
-var storeLocator=document.querySelector("#storeLocator");
-storeLocator.addEventListener("click", storeLocatorPage)
-function storeLocatorPage(){
-  window.location.href="storeLocator.html";
- 
-}
-console.log("Hello");
+bodydata=[
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Body Lotion with Vitamin C and Honey for Radiant Skin - 400 ml",
+   rating:"4.8★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Body Lotion with Vitamin C & Honey for Radiant Skin - 400 ml ",
+   rating:"4.7★",
+   price:"₹349"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/p/e/perfume-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Mamaearth ME Eau De Parfum For a Fragrance As Unique As You - 50 ml",
+   rating:"4.8★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/r/o/rose_body_lotion_7.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Rose Body Lotion with Rose Water & Milk for Deep Hydration (400 ml)",
+   rating:"4.8★",
+   price:"₹499"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan-cold-cream-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Nourishing Cold Cream with Turmeric & Saffron for Glowing",
+   rating:"4.4★",
+   price:"₹299"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/c/o/coco_hand_cream_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"CoCo Hand Cream with Coffee and Cocoa for Rich Moisturization - 50g",
+   rating:"4.4★",
+   price:"₹699"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/t/e/tea-tree-body-wash-1_1_vzbz9azcbbuefo72.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Tea Tree Body Wash With Tea Tree and Neem For Skin Purification - 300 ml",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/t/e/tea-tree-body-wash-1_1_vzbz9azcbbuefo72.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Tea Tree Body Wash With Tea Tree and Neem For Skin Purification - 300 ml",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-_-charcoal-body-wash.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Body Wash Combo: Vitamin C Body Wash and Charcoal Body Wash",
+   rating:"4.7★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-_-charcoal-body-wash.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Body Wash Combo: Vitamin C Body Wash and Charcoal Body Wash",
+   rating:"4.7★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Body Lotion with Vitamin C and Honey for Radiant Skin - 400 ml",
+   rating:"4.8★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Body Lotion with Vitamin C & Honey for Radiant Skin - 400 ml ",
+   rating:"4.7★",
+   price:"₹349"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/p/e/perfume-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Mamaearth ME Eau De Parfum For a Fragrance As Unique As You - 50 ml",
+   rating:"4.8★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/r/o/rose_body_lotion_7.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Rose Body Lotion with Rose Water & Milk for Deep Hydration (400 ml)",
+   rating:"4.8★",
+   price:"₹499"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan-cold-cream-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Nourishing Cold Cream with Turmeric & Saffron for Glowing",
+   rating:"4.4★",
+   price:"₹299"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/c/o/coco_hand_cream_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"CoCo Hand Cream with Coffee and Cocoa for Rich Moisturization - 50g",
+   rating:"4.4★",
+   price:"₹699"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/t/e/tea-tree-body-wash-1_1_vzbz9azcbbuefo72.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Tea Tree Body Wash With Tea Tree and Neem For Skin Purification - 300 ml",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-_-charcoal-body-wash.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Body Wash Combo: Vitamin C Body Wash and Charcoal Body Wash",
+   rating:"4.7★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Body Lotion with Vitamin C & Honey for Radiant Skin - 400 ml ",
+   rating:"4.7★",
+   price:"₹349"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/p/e/perfume-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Mamaearth ME Eau De Parfum For a Fragrance As Unique As You - 50 ml",
+   rating:"4.8★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Body Lotion with Vitamin C & Honey for Radiant Skin - 400 ml ",
+   rating:"4.7★",
+   price:"₹349"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/p/e/perfume-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Mamaearth ME Eau De Parfum For a Fragrance As Unique As You - 50 ml",
+   rating:"4.8★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/r/o/rose_body_lotion_7.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Rose Body Lotion with Rose Water & Milk for Deep Hydration (400 ml)",
+   rating:"4.8★",
+   price:"₹499"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/u/b/ubtan-cold-cream-1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Ubtan Nourishing Cold Cream with Turmeric & Saffron for Glowing",
+   rating:"4.4★",
+   price:"₹299"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/c/o/coco_hand_cream_1.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"CoCo Hand Cream with Coffee and Cocoa for Rich Moisturization - 50g",
+   rating:"4.4★",
+   price:"₹699"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/t/e/tea-tree-body-wash-1_1_vzbz9azcbbuefo72.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Tea Tree Body Wash With Tea Tree and Neem For Skin Purification - 300 ml",
+   rating:"4.9★",
+   price:"₹599"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-_-charcoal-body-wash.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Body Wash Combo: Vitamin C Body Wash and Charcoal Body Wash",
+   rating:"4.7★",
+   price:"₹399"
+  },
+  {image:"https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion.jpg?auto=format&fit=crop&w=683&auto=compress",
+   name:"Vitamin C Body Lotion with Vitamin C & Honey for Radiant Skin - 400 ml ",
+   rating:"4.7★",
+   price:"₹349"
+  },
+
+];
+bodydata.map(function(elem){
+  var div=document.createElement("div")
+  var img=document.createElement("img");
+  img.src=elem.image;
+  var h3=document.createElement("p");
+  h3.innerText=elem.name;
+  var rating=document.createElement("div")
+  rating.innerText=elem.rating;
+  var price=document.createElement("h3")
+  price.innerText=elem.price;
+  var btn=document.createElement("button")
+  btn.innerText="ADD TO CART";
+  btn.setAttribute("class","btn")
+  div.append(img,h3,rating,price,btn)
+  document.querySelector("#container").append(div);
+
+
+});
