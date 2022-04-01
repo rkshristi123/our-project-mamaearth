21a388f653da0c699958a0a5e50f7623f664b57d
Chandani99 <er.chandni99@gmail.com>
2022-04-01 18:17:01 +0530

    cart js page added

---
+++ cart.js
@@ -0,0 +1,5 @@
+var procedTopay=document.querySelector("#proceedPay");
+    procedTopay.addEventListener("click", paymentpage);
+    function paymentpage(){
+        window.location.href="paymentPage.html";
+    }
