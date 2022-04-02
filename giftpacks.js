e20fba93e9ebe870c10ddee821ce8fbb0d89985a
Chandani99 <er.chandni99@gmail.com>
2022-04-02 10:43:24 +0530

    git page modified

--- giftpacks.js
+++ giftpacks.js
@@ -2,7 +2,8 @@ var giftpacks = [
     {
         image:
         "https://honasa-mamaearth-production.imgix.net/a/g/agent-apple-body-wash.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"5.0 ★",
         price:"299",
         discount:"₹598 |",
         off:"(50% off)"
@@ -10,7 +11,8 @@ var giftpacks = [
         {
         image:
         "https://honasa-mamaearth-production.imgix.net/m/a/mango-body-wash.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Major Mango  Body Wash for Kids with Mango & Oat Protine -300ml (Pack of 2)",
+        name: "Major Mango  Body Wash for Kids with Mango & Oat Protine -300ml (Pack of 2)",
+        rating:"5.0 ★",
         price:"299",
         discount:"₹598 |",
         off:"(50% off)"
@@ -18,152 +20,168 @@ var giftpacks = [
         {
          image:
         "https://honasa-mamaearth-production.imgix.net/s/u/super-strawberry-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Super Strawbery Body Lotion for Kids With Strawbary & Shea Butter -400ml (Pack of 2)",
-        price:"₹399",
+        name: "Super Strawbery Body Lotion for Kids With Strawbary & Shea Butter -400ml (Pack of 2)",
+        rating:"5.0 ★",
+        price:"399",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/o/r/original-orange-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Original Orange Body Lotion For Kids With Orange & Shea Butter -400ml (Pack of 2)",
-        price:"₹399 ",
+        name: "Original Orange Body Lotion For Kids With Orange & Shea Butter -400ml (Pack of 2)",
+        rating:"5.0 ★",
+        price:"399 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/m/a/major-mango-body-lotion-for-kids.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Major Mango Body Lotion For Kids With Butter & Milk Protine -400ml (Pack of 2)",
-        price:"₹399 ",
+        name: "Major Mango Body Lotion For Kids With Butter & Milk Protine -400ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"399 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
-       image:
-        "https://honasa-mamaearth-production.imgix.net/a/g/agent-apple-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Lotion For kids With Apple & Oat Milk  -400ml (Pack of 2)",
-        price:"₹399 ",
+       image:"https://honasa-mamaearth-production.imgix.net/a/g/agent-apple-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
+        name: "Agent Apple Body Lotion For kids With Apple & Oat Milk  -400ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"399 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
-       image:
-        "https://honasa-mamaearth-production.imgix.net/p/e/perfume-1_3.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Mameearth ME Eau De Perfume For a Fragnance As Unioque As You 8ml Kit",
-        price:"₹399",
+       image:"https://honasa-mamaearth-production.imgix.net/p/e/perfume-1_3.jpg?auto=compress&fit=scale&w=400&h=400",
+        name: "Mameearth ME Eau De Perfume For a Fragnance As Unioque As You 8ml Kit",
+        rating:"4.9 ★",
+        price:"399",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
-       image:
-        "https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_and_ubtan.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Skin Redians Body Care Kit-Unatn Body Lotion & Viatamin  -400ml (Pack of 2)",
-        price:"₹518",
+       image: "https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_and_ubtan.jpg?auto=compress&fit=scale&w=400&h=400",
+       name: "Skin Redians Body Care Kit-Unatn Body Lotion & Viatamin  -400ml (Pack of 2)",
+       rating:"4.9 ★",
+       price:"518",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_and_rose.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Skin Refreshing & Smooth Body Care Kit-Rose Body Lotion -400ml (Pack of 2)",
-        price:"₹518 ",
+        name: "Skin Refreshing & Smooth Body Care Kit-Rose Body Lotion -400ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"518 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/u/b/ubtan_and_rose.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Skin Nourishing Body Care Cambo - Ubten Body lotion -400ml (Pack of 2)",
-        price:"₹518 ",
+        name: "Skin Nourishing Body Care Cambo - Ubten Body lotion -400ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"518 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/v/i/vitamin_c_body_lotion.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "ubtan Body Lotion with Turmeric & Kokum Butter -400ml (Pack of 2)",
-        price:"₹518 ",
+        name: "ubtan Body Lotion with Turmeric & Kokum Butter -400ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"518 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/u/b/ubtan_body_lotion_7.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Rose Body Lotion with Rose Water Milk for Deep Hydration -400ml (Pack of 2)",
-        price:"₹518 ",
+        name: "Rose Body Lotion with Rose Water Milk for Deep Hydration -400ml (Pack of 2)",
+        rating:"5.0 ★",
+        price:"518 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/u/b/ubtan_body_lotion_7.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2) ",
-        price:"₹518 ",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2) ",
+        rating:"5.0 ★",
+        price:"518 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/r/o/rose_body_lotion_7.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2) ",
-        price:"₹1123 ",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2) ",
+        rating:"5.0 ★",
+        price:"1123 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/c/o/combo_4.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹768 ",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"768 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/a/n/anti_hair_fall_spa_kit_1.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹1187 ",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.8 ★",
+        price:"1187 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/a/n/anti-fall_regimen_kit_uges0reark90qp1c.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹803 ",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"803 ",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/1/2/1200x1200_poqqaktkr8d9andg.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2) ",
-        price:"₹999",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2) ",
+        rating:"4.9 ★",
+        price:"999",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/u/b/ubtan_body_lotion_7.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "ubtan Body Lotion with Turmeric & Kokum Butter  -400ml (Pack of 2)",
-        price:"₹997",
+        name: "ubtan Body Lotion with Turmeric & Kokum Butter  -400ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"997",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/h/a/hair_fall_control_kit_1.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹999",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"999",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/c/o/combo_4.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹908",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"908",
         discount:"₹598 |",
         off:"(50% off)"
 
@@ -171,16 +189,18 @@ var giftpacks = [
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/w/i/winter-essential-kit_84fel26stplkpxsv.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹999",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.9 ★", 
+        price:"999",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/d/a/damage-repair-kit.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹768",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"5.0 ★",
+        price:"768",
         discount:"₹598 |",
         off:"(50% off)"
         },
@@ -188,39 +208,46 @@ var giftpacks = [
        image:
         "https://honasa-mamaearth-production.imgix.net/w/e/wedding-kit-_1200x1200_1_gihldmqzgk4fkgsh.jpg?auto=compress&fit=scale&w=400&h=400",
         name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹942",
+        rating:"5.0 ★",
+        price:"942",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/b/h/bhringamla-combo_msqe7ulfgyv85fsi.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹838",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"838",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/v/i/vitamin-c-skincare-regimen-kit_mzka1kswpz0gem40.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹949",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"5.0 ★",
+        price:"949",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/t/a/tan-removal-regimen_10_whntawimnjpocnz9.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹768",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"5.0 ★",
+        price:"768",
         discount:"₹598 |",
         off:"(50% off)"
         },
         {
        image:
         "https://honasa-mamaearth-production.imgix.net/b/a/baby-wipes-_72-3_.jpg?auto=compress&fit=scale&w=400&h=400",
-        about: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
-        price:"₹649",
+        name: "Agent Apple Body Wash for Kids with Apple & Oat Protine -300ml (Pack of 2)",
+        rating:"4.9 ★",
+        price:"649",
+        discount:"₹1098 |",
+        off:"(50% off)"
         },
         
         
@@ -243,7 +270,7 @@ var addToCart=JSON.parse(localStorage.getItem("Cart"))||[];
 
 // console.log(products)
 document.querySelector("#box").innerHTML = "";
-giftpacksvar.map(function (elem){
+giftpacks.map(function (elem){
 var  box = document.createElement("div");
 box.setAttribute("class","maindiv")
 
@@ -267,22 +294,6 @@ rate.setAttribute("class","rating")
 rate.innerText = elem.rating;
 rate.style.color = "white"
 
-console.log(rate)
-// rate.style.background = "green"
-
-
-// var price = document.createElement("p");
-// price.innerText = elem.price;
-// // price.style.textDecoration = "line-through"
-
-// var btn = document.createElement("button");
-// btn.setAttribute("class","btn")
-// btn.innerText = " ADD TO CART";
-
-
-// btn.addEventListener("click",function(){
-//   addToCart(elem);
-// });
 var priceTag=document.createElement("div");
 priceTag.style.display="flex";
 priceTag.style.justifyContent="center";
@@ -295,6 +306,26 @@ var price=document.createElement("h3")
 price.innerText=elem.price;
 
 priceTag.append(currency,price);
+
+
+var strikedprice = document.createElement("p");
+strikedprice.innerText = elem.discount;
+strikedprice.style.textDecoration="line-through";
+strikedprice.style.color="gray";
+
+var off = document.createElement("p");
+off.innerText = elem.off;
+off.style.color="red";
+ 
+priceTag.setAttribute("class","pr")
+var allPrice=document.createElement("div");
+allPrice.style.display="flex";
+allPrice.style.width="80%";
+allPrice.style.marginLeft="10%";
+allPrice.style.fontSize="16px";
+allPrice.style.marginTop="20px";
+allPrice.style.justifyContent="space-around";
+allPrice.append(priceTag,strikedprice,off);
 
 var btn=document.createElement("button")
 btn.innerText="ADD TO CART";
@@ -309,7 +340,7 @@ localStorage.setItem("mamaEarthCartCount",count);
 displayCount.innerText=count;
 });
 
-box.append(img,parag,rate,priceTag,btn);
+box.append(img,parag,rate,allPrice,btn);
 
 document.querySelector("#box").append(box);
 
