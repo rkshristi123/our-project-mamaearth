01d924f24fb8daf4ddb2b968dc5aa02d18c8935d
Chandani99 <er.chandni99@gmail.com>
2022-04-02 23:19:32 +0530

    payment js added

--- payment.js
+++ payment.js
@@ -7,3 +7,45 @@ var count=localStorage.getItem("mamaEarthCartCount")||0;
         var allprice=cartproducts[i].price;
         totalprice=totalprice+Number(allprice);
     }
+
+var totalprice=localStorage.getItem("mamaearthTotalPrice");
+    var orderedItems=JSON.parse(localStorage.getItem("Orders")) ||[];
+
+    document.querySelector("form").addEventListener("submit", checkoutFunction);
+    function checkoutFunction(){
+        event.preventDefault();
+        var cardNumber=document.querySelector("#number").value;
+        var cardno = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+        if(cardNumber==" " || cardNumber.length!=16){
+            alert("Not a valid Visa credit card number!");
+        }
+        var cvv=document.querySelector("#cvv").value;
+        if(cvv==" " || cvv.length!=3 ){
+            alert("Not a valid cvv number!"); 
+        }
+        var expirydate=document.querySelector("#exp").value;
+        if(expirydate==" " ){
+            alert("Plese enter the date");
+        }
+        var name=document.querySelector("#name").value;
+        if(name==" " ){
+            alert("Plese enter the Card Holder Name");
+       }
+      var otp=prompt("The OTP has bees sent to your registered mob. number, Please enter the otp");
+      if(otp==1234){
+            addedIntoKart.map(function(elem){
+                orderedItems.push(elem);
+                return orderedItems;
+
+            })
+            console.log(orderedItems);
+            //Storing data on local storage
+            localStorage.setItem("Orders",JSON.stringify(orderedItems));
+            alert("Congratulation! You have successfully registered.");
+            window.open("index.html");
+        
+        }
+        else{
+            alert("Oops! you have entered wrong otp");
+        }
+    }
